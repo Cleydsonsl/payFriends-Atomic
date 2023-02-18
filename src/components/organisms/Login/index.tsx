31471fd52abceef91335892144d68ee54aa99fde
cleydson souza <cley.souza007@gmail.com>
2023-02-18 17:30:01 -0300

    feat: create a new styledComponent for using in form

--- src/components/organisms/Login/index.tsx
+++ src/components/organisms/Login/index.tsx
@@ -2,26 +2,29 @@ import { Image } from "@app/components/atoms/Image";
 import { Text } from "@app/components/atoms/Text";
 import { Brand } from "@app/components/molecules/Brand";
 import { FormLogin } from "@app/components/molecules/FormLogin";
-import { Container, ContentBrand, ContentForm } from "./styles";
+import { Container, ContentBrand, ContentForm, ContentTitle } from "./styles";
 
 export function LoginOr() {
   return(
     <Container>
       <ContentForm>
-        <Image 
-          src="/PayFriends.svg" 
-          alt="payfriends" 
-          width={165}
-          height={39}
-        />
-        <Text 
-          text="Bem-vindo de volta"
-          styles={{
-            fontSize: "36px", 
-            fontWeight: "700",
-            color: "#4E4E4E"
-          }}
-        />
+        <ContentTitle>
+          <Image 
+            src="/PayFriends.svg" 
+            alt="payfriends" 
+            width={165}
+            height={39}
+          />
+          <Text 
+            text="Bem-vindo de volta"
+            styles={{
+              fontSize: "36px",
+              fontWeight: "700",
+              color: "#4E4E4E"
+            }}
+          />
+        </ContentTitle>
+        
         <FormLogin/>
       </ContentForm>
       <ContentBrand>
--- src/components/organisms/Login/styles.ts
+++ src/components/organisms/Login/styles.ts
@@ -24,3 +24,13 @@ export const ContentBrand = styled.span`
   justify-content: center;
   background: ${theme.colors.shape};
 `;
+
+export const ContentTitle = styled.span`
+  width: 100%;
+  min-height: 100px;
+  display: flex;
+  align-items: flex-start;
+  justify-content: space-between;
+  flex-direction: column;
+  margin-bottom: 80px;
+`;
