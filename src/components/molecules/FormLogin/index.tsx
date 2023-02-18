ae25ef09afcee7f888ebf8baca2fcd297b6a17f6
cleydson souza <cley.souza007@gmail.com>
2023-02-18 17:28:34 -0300

    feat: using a new component

--- src/components/molecules/FormLogin/index.tsx
+++ src/components/molecules/FormLogin/index.tsx
@@ -1,15 +1,21 @@
 import { Button } from "@app/components/atoms/Button";
+import { Icon } from "@app/components/atoms/Icon";
 import { Input } from "@app/components/atoms/Input";
 import { Container, ContentInput } from "./styles";
+import { FaUser } from "react-icons/fa";
+import { FaLock } from "react-icons/fa";
+
 
 export function FormLogin() {
   return(
     <Container>
       <ContentInput>
-        <Input placeholder="Digite seu email" value=""/>
+        <Icon icon={FaUser} color="#D1D1D1" size={18}/>
+        <Input placeholder="Email" value=""/>
       </ContentInput>
       <ContentInput>
-        <Input placeholder="Digite seu email" value=""/>
+        <Icon icon={FaLock} color="#D1D1D1" size={18}/>
+        <Input placeholder="Senha" value=""/>
       </ContentInput>
       <Button text="ENTRAR" />
     </Container>
